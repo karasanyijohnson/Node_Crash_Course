412b51803d36c03e9a2e62bdf25e838fd5e3f207
Johnson <johnsonkarasanyi@gmail.com>
2022-05-23 17:57:58 +0200

    concatenate paths

--- reference/path_demo.js
+++ reference/path_demo.js
@@ -8,11 +8,11 @@ const path = require('path');
 // console.log(path.dirname(__filename))
 
 //File extension
- console.log(path.extname(__filename))
+ //console.log(path.extname(__filename))
 
 // Create path object
 // console.log(path.parse(__filename))
 // console.log(path.parse(__filename).base)
 
 // Concatenate paths
-//console.log(path.join(__dirname,'test',"hello.html"))
+console.log(path.join(__dirname,'test',"hello.html"))
