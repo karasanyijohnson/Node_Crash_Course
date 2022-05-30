dd10f9f31b4fe64965abe05f5707a45d91378272
Johnson <johnsonkarasanyi@gmail.com>
2022-05-30 14:05:59 +0200

    events module

--- index.js
+++ index.js
@@ -3,6 +3,13 @@
 // console.log(person.name)
 
 // THIS METHOD IS CALLED COMMON JS
-const Person = require('./person')
-const person1 = new Person("Johnson", 30)
-person1.greeting()
+// const Person = require('./person')
+// const person1 = new Person("Johnson", 30)
+// person1.greeting()
+
+const Logger = require('./logger');
+const logger = new Logger();
+logger.on('Message', data=>console.log('Called Listener', data))
+logger.log("Hello World");
+logger.log("Hi ");
+logger.log("Hello ")
